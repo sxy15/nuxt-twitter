a26f798501c9404e2ff5036484a325343b57e98d
sxy15 <sxy_19921115@163.com>
2023-09-22 23:24:29 +0800

    feat: left sidebar

--- app.vue
+++ app.vue
@@ -1,5 +1,31 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div :class="{ dark: darkMode }">
+    <div class="bg-white dark:bg-dim-900">
+      <div class="min-h-full">
+        <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
+          <!-- left sidebar -->
+          <div class="hidden md:block xs:col-span-1 xl:col-span-2">
+            <div class="sticky top-0">
+              <sidebar-left></sidebar-left>
+            </div>
+          </div>
+          <!-- main content -->
+          <main class="col-span-12 md:col-span-8 xl:col-span-6">
+            main
+          </main>
+          <!-- right sidebar -->
+          <div class="hidden col-span-12 md:block xl:col-span-4 md:col-span-3">
+            <div class="sticky top-0">
+              right
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+const darkMode = ref(false)
+
+</script>
