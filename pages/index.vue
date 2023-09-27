8595cefc965c0f5b1e8afc4cc77b23ce84d50e3f
sxy15 <sxy_19921115@163.com>
2023-09-27 22:48:14 +0800

    feat: server

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,21 @@
 </template>
 
 <script setup lang="ts">
+import { useFetch } from 'nuxt/app';
 import { ref, onMounted } from 'vue'
 
 const loading = ref(false)
+
+onMounted(() => {
+  useFetch('/api/auth/register', {
+    method: 'POST',
+    body: {
+      username: 'username',
+      email: 'x',
+      password: 'x',
+      name: 'x',
+      repeatPassword: 'x'
+    }
+  })
+})
 </script>
